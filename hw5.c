7a1c6d5ac71f52815313c2f19f9c12659195ff17
kristerssaulitis <kristers.saulitis1@gmail.com>
2021-11-14 13:37:29 +0200

    shared memmory buffer example

--- hw5.c
+++ hw5.c
@@ -6,14 +6,20 @@
 #define MAXSIZE 1024
 #define MAX_LENGTH 256
 
-char* chunks;
-char* sizes;
-int* buffer;
+char* chunkFileName;
+char* sizeFileName;
+int* buffer[1024];
 char key[256];
 
 char* shared_memory = NULL;
-int* client_count = NULL;
+int* chunkTotal = NULL;
+int* chunkBuffer = NULL;
+int* chunkPointer = NULL;
 int* shared_data = NULL;
+
+
+int chunkCount = 0;
+
 
 int chechInput(int argc, char** argv){
     if (argc < 2){
@@ -34,35 +40,45 @@ int chechInput(int argc, char** argv){
     char *randomchunks = argv[1];
     char *ranodmsizes = argv[2];
 
-    for (i =0; i< 2; i++) chunks = strsep(&randomchunks,"=");
-    for (i =0; i< 2; i++) sizes = strsep(&ranodmsizes,"=");
+    for (i =0; i< 2; i++) chunkFileName = strsep(&randomchunks,"=");
+    for (i =0; i< 2; i++) sizeFileName = strsep(&ranodmsizes,"=");
 
-    printf("this is chunk :%s and this is size:%s      \n", chunks, sizes);
+    printf("this is chunk :%s and this is size:%s      \n", chunkFileName, sizeFileName);
     return 0;
 }
 
 int readChunks(FILE *f, char *key){
     if (!f) return -1;
-
+    int i = 0;
     while (fgets(key, MAX_LENGTH, f)){
-        printf("%s", key);
+        buffer[i] = atoi(key);
+        printf("%i \n", buffer[i]);
+        i++;
     }
+
     /* Close file */
     fclose(f);
 }
 
-int createBuffer(int chunkcount){
-    shared_memory = mmap(NULL, MAXSIZE-chunkcount, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
-    client_count = (int*) shared_memory;
-    shared_data = (int*) (shared_memory + chunkcount);
+int createBuffer(int chunkCount){
+    int chunkCount = sizeof(buffer)/sizeof(int);
+    /* the buffer is 1024, however, we have to take into account the bookkeping info
+                        chunk count +      chunks                        chunk pointer
+                    example:  5 chunks     |10|222|3|4567|5000|              |pointer to 10 in the shared memory| pointer to 222 in the .....|||||
+    */
+    int memoryBeforeShared = sizeof(int) + (chunkCount * sizeof(int)) + (chunkCount * sizeof(int*));
+    shared_memory = mmap(NULL, MAXSIZE-memoryBeforeShared, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    chunkTotal = (int*) shared_memory;
+    chunkBuffer = (int*) (shared_memory + sizeof(chunkTotal)); /*sizeof(chunkTotal) = 4*/
+    chunkPointer = (int*) (shared_memory + sizeof(chunkTotal) + sizeof(chunkBuffer));
+    shared_data = (int*) (shared_memory + sizeof(chunkTotal) + sizeof(chunkBuffer) + sizeof(chunkPointer));
 }
 
 int main(int argc, char** argv){
-    buffer = malloc(MAXSIZE);
-
+    printf("size of int pointer %lu \n", sizeof(char*)); /* all pointers are 8 bytes long */
     chechInput(argc, argv);
 
-    FILE *f = fopen(chunks, "r");
+    FILE *f = fopen(chunkFileName, "r");
     readChunks(f, key);
     size_t n = sizeof(key) / sizeof(key[0]);
     createBuffer(n);
